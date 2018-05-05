58660918f9bc7c29fbd8eaf483016df0b1fbd7e4
Adam Goldberg <gold.adamb@gmail.com>
2018-05-05 11:27:52 +1000

    layout

--- src/pages/event.js
+++ src/pages/event.js
@@ -2,9 +2,23 @@ import React from 'react'
 import { withStyles } from 'material-ui/styles'
 import withRoot from '../withRoot'
 import Events from '../eventData'
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const styles = theme => ({
-  root: {}
+  roots: {
+        border: '1px'
+  },
+  grid: {
+    margin: '10%'
+    // marginLeft: '5%',
+    // marginRight: '5%'
+  },
+  box: {
+    color: 'blue',
+    border: 'solid 1px black',
+    margin: '10px',
+    padding: '10%'
+  }
 })
 
 class Event extends React.Component {
@@ -13,15 +27,61 @@ class Event extends React.Component {
     const { classes, eventID } = this.props
     const EventData = Events[eventID]
 
+    // return (
+    //   <div className={classes.root}>
+    //     <h1>{EventData.name}</h1>
+    //     <h3>{EventData.description}</h3>
+    //     <h3>start: {EventData.start_date}</h3>
+    //     <h3>end: {EventData.end_date}</h3>
+    //     <h3>related companies: {EventData.related_companies.join(', ')}</h3>
+    //   </div>
+    // );
     return (
-      <div className={classes.root}>
-        <h1>{EventData.name}</h1>
-        <h3>{EventData.description}</h3>
-        <h3>start: {EventData.start_date}</h3>
-        <h3>end: {EventData.end_date}</h3>
-        <h3>related companies: {EventData.related_companies.join(', ')}</h3>
-      </div>
-    )
+
+      <Grid fluid className={classes.grid}>
+        <Row className={classes.row} around="xs">
+          <Col className={classes.box} xs>
+            description
+          </Col>
+         </Row>
+
+         <Row className={classes.row} around="xs">
+          <Col className={classes.box} xs>
+            Effected Companie 1
+          </Col>
+          <Col className={classes.box} xs>
+            Effected Companie 2
+          </Col>
+          <Col className={classes.box} xs>
+            Effected Companie 3
+          </Col>
+        </Row>
+
+        <Row className={classes.row} around="xs">
+         <Col className={classes.box} xs>
+           Stocks
+         </Col>
+         <Col className={classes.box} xs>
+            Maps
+         </Col>
+        </Row>
+        <Row className={classes.row} around="xs">
+            <Col className={classes.box} xs>
+                Stocks
+            </Col>
+        </Row>
+        <Row className={classes.row} around="xs">
+            <Col className={classes.box} xs>
+                Stocks
+            </Col>
+        </Row>
+        <Row className={classes.row} around="xs">
+            <Col className={classes.box} xs>
+                Stocks
+            </Col>
+        </Row>
+    </Grid>
+    );
   }
 }
 
