b40c497e0f2eb9ff7592f4d838999b459e67345d
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-05-05 13:28:18 +1000

    fill in timeline with more event data

--- src/eventData.js
+++ src/eventData.js
@@ -1,37 +1,103 @@
-// more keys: wZBNu9hY, 7HTAkxj7, r7F3SepB
+/*
+ * dates are unix timestamps
+ * keywords used for news article search
+ * names, descriptions and companies pre-determined
+ */
+
 const Events = {
   qcHY624B: {
-    name: 'Samsung Battery Scandal',
-    description: 'They were exploding!',
-    // assume dates are unix time stamps
-    start_date: 1112893624595,
-    end_date: 1496302995,
+    name: 'Samsung battery scandal',
+    description: 'Samsung share prices plummet after Galaxy Note 7 batteries cause explosions',
+    start_date: 1471528800, /* release date */
+    end_date: 1485003600, /* public apology */
     related_companies: [
       'Samsung'
-    ]
+    ],
+    keywords: ['samsung', 'battery']
+  },
+  wZBNu9hY: {
+    name: 'Royal Commission into banking',
+    description: 'The Commonwealth launches a royal commision into the financial services industry to investigate allegations of misconduct',
+    start_date: 1511960400, /* royal commission announced */
+    end_date: 1517317200, /* royal commission began */
+    related_companies: [
+      'AMP',
+      'CBA',
+      'Westpac',
+      'ANZ',
+      'NAB',
+      'BT Financial',
+      'Aussie Home Loans',
+      'St George'
+    ],
+    keywords: ['banking', 'royal', 'commission']
+  },
+  p3TAkxj7: {
+    name: 'Malaysia Airlines scandal',
+    description: 'Future of Malaysia Airlines uncertain after Flight MH370 missing and Flight MH17 shot down',
+    start_date: 1394197200, /* date of crash */
+    end_date: 1425474000, /* search for mh370 cancelled */
+    related_companies: [
+      'Malaysia Airlines'
+    ],
+    keywords: ['malaysia airlines', 'mh370', 'mh17']
+  },
+  r7F3SepB: {
+    name: 'Elon Musk dismissive of analysts',
+    description: 'Elon Musk\'s refusal to answer \'boring\' questions sinks Tesla shares',
+    start_date: 1525010400, /* initial instance */
+    end_date: 1530280800, /* ongoing - end date in future */
+    related_companies: [
+      'Tesla'
+    ],
+    keywords: ['tesla', 'elon musk', 'analysts']
   },
   Kr3muXpn: {
-    name: 'Cambridge Analytica',
-    description: 'They took our data',
-    // assume dates are unix time stamps
-    start_date: 1493924595,
-    end_date: 1497602995,
+    name: 'Cambridge Analytica scandal',
+    description: 'Facebook in the spotlight after Cambridge Analytica involved in alleged harvesting and use of personal data',
+    start_date: 1519822800, /* scandal made public */
+    end_date: 1530280800, /* ongoing - end date in future */
     related_companies: [
       'Facebook',
       'Cambridge Analytica'
-    ]
+    ],
+    keywords: ['facebook', 'cambridge', 'analytica']
   },
-  '78Mhx8UT': {
-    name: 'Another Title',
-    description: 'Some random text!',
-    // assume dates are unix time stamps
-    start_date: 1112893624595,
-    end_date: 1496302995,
+  r8Mhx8UT: {
+    name: 'James Packer resigns from Crown resorts directorship',
+    description: 'Crown stock plummeted instantly on open of trading after CEO resigned due to mental health issues',
+    start_date: 1520168400, /* disciplinary action against crown */
+    end_date: 1522328400, /* past date of scandal */
     related_companies: [
-      'Google',
-      'Microsoft'
-    ]
+      'Crown Resorts',
+      'PBL'
+    ],
+    keywords: ['james packer', 'crown resorts', 'resigns', 'mental health']
+  },
+  /*
+  g3F6aUb7: {
+    name: '',
+    description: '',
+    start_date: ,
+    end_date: ,
+    related_companies: [
+      '',
+      ''
+    ],
+    keywords: ['', '', '']
+  },
+  Hf3dG22P: {
+    name: '',
+    description: '',
+    start_date: ,
+    end_date: ,
+    related_companies: [
+      '',
+      ''
+    ],
+    keywords: ['', '', '']
   }
+  */
 }
 
 export default Events
