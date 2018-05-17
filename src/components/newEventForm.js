e78d9f83927c41b53d9c3ea7da43b6c9af8038cb
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-05-18 09:51:11 +1000

    form finished. need tro make submit work

--- src/components/newEventForm.js
+++ src/components/newEventForm.js
@@ -1,22 +1,49 @@
 import React from 'react'
 import { withStyles } from 'material-ui/styles'
 import withRoot from '../withRoot'
+import IconButton from '@material-ui/core/IconButton'
+import { Delete } from 'material-ui-icons'
 import { fb } from '../config'
+import _ from 'lodash'
+import Typography from 'material-ui/Typography'
 import PropTypes from 'prop-types'
 import Dialog, {
   DialogContent,
-  DialogTitle
+  DialogTitle,
+  DialogActions
 } from 'material-ui/Dialog'
-import Button from 'material-ui/Button'
 
-const styles = {
-}
+import ChipInput from 'material-ui-chip-input'
+import Button from 'material-ui/Button'
+import TextField from 'material-ui/TextField'
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    marginTop: theme.spacing.unit,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+})
 
 class NewEventForm extends React.Component {
 
   static propTypes = {
     isOpen: PropTypes.bool.isRequired,
     closeCallback: PropTypes.func.isRequired
+  }
+
+  state = {
+    name: '',
+    description: '',
+    start_date: '',
+    end_date: '',
+    related_companies: {},
+    keywords: [],
+    companyCode: '',
+    companyName: ''
   }
 
   handleClose = () => {
@@ -36,19 +63,149 @@ class NewEventForm extends React.Component {
       related_companies: {
         'ben': 'rohald'
       }
-    });
+    })
 
-    this.handleClose()
+    this.handleClcose()
   }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    })
+  }
+
+  removeChip = (chip, i) => {
+    let newKeyWords = this.state.keywords
+    newKeyWords.splice(i,1)
+    this.setState({keywords: newKeyWords})
+  }
+
+  addChip = (chip) =>
+    this.state.keywords.length < 4 ? this.setState({keywords: [...this.state.keywords, chip]}) : null
+
+  addNewCompany = () => {
+    let related_companies = this.state.related_companies
+    related_companies[this.state.companyName] = this.state.companyCode
+    this.setState({
+      related_companies,
+      companyName: '',
+      companyCode: ''
+    })
+  }
+
+  deleteRelatedCompany = (key) => {
+    let related_companies = this.state.related_companies
+    delete related_companies[key]
+    this.setState({related_companies})
+  }
+  
 
   render () {
     const { classes } = this.props
+    const { related_companies } = this.state
 
     return (
       <Dialog open={this.props.isOpen} onClose={this.handleClose} aria-labelledby="simple-dialog-title" >
-        <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
-        <DialogContent> 
-          <Button onClick={() => this.addEvent()}>Add Event</Button>
+        <DialogTitle id="simple-dialog-title">Create New Event</DialogTitle>
+        <DialogContent>
+          <form>
+            <TextField
+              id="name"
+              label="Name"
+              className={classes.textField}
+              value={this.state.name}
+              onChange={this.handleChange('name')}
+              margin="normal"
+              fullWidth
+            />
+
+            <TextField
+              id="description"
+              label="Description"
+              multiline
+              fullWidth
+              className={classes.textField}
+              value={this.state.description}
+              onChange={this.handleChange('description')}
+              margin="normal"
+            />
+            <TextField
+              label="Start Date"
+              type="date"
+              value={this.state.start_date}
+              onChange={this.handleChange('start_date')}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <TextField
+              label="End Date"
+              type="date"
+              value={this.state.start_date}
+              onChange={this.handleChange('end_date')}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+
+            <ChipInput
+              label="Keywords"
+              value={this.state.keywords}
+              onAdd={(chip) => this.addChip(chip)}
+              className={classes.textField}
+              fullWidth
+              onDelete={(chip, i) => this.removeChip(chip, i)}
+            />
+
+            <div className={classes.textField}>
+              <Typography gutterBottom variant="subheading">
+                Related Companies:
+              </Typography>
+              {_.map(_.keys(related_companies), (key, i) => (
+                <div key={i}>
+                  <Typography style={{display: 'inline'}}>
+                    {key}, {related_companies[key]}
+                  </Typography>
+                  <IconButton 
+                    className={classes.button} aria-label="Delete"
+                    onClick={() => this.deleteRelatedCompany(key)}
+                  >
+                    <Delete/>
+                  </IconButton>
+                </div>
+              ))}
+            </div>
+
+            <TextField
+              label="Company Name"
+              className={classes.textField}
+              value={this.state.companyName}
+              onChange={this.handleChange('companyName')}
+              margin="normal"
+            />
+            <TextField
+              label="Stock Code"
+              className={classes.textField}
+              value={this.state.companyCode}
+              onChange={this.handleChange('companyCode')}
+              margin="normal"
+            />
+            <Button
+              size="small"
+              onClick={() => this.addNewCompany()}
+            >
+              Add
+            </Button>
+          </form>
+          <DialogActions>
+            <Button size="medium" color="secondary" 
+              onClick={() => this.addEvent()}
+            >
+              Add Event
+            </Button>
+          </DialogActions>
         </DialogContent>
       </Dialog>
     )
