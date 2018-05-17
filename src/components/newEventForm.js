e4a40bf07231dbc8435ec9c8f1a7583db4f3eee9
rohaldbUni <z5019999@ad.unsw.edu.au>
2018-05-17 18:26:29 +1000

    can add firebase data

--- src/components/newEventForm.js
+++ src/components/newEventForm.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { withStyles } from 'material-ui/styles'
 import withRoot from '../withRoot'
+import { fb } from '../config'
 import PropTypes from 'prop-types'
 import Dialog, {
   DialogContent,
   DialogTitle
 } from 'material-ui/Dialog'
+import Button from 'material-ui/Button'
 
 const styles = {
 }
@@ -21,12 +23,33 @@ class NewEventForm extends React.Component {
     this.props.closeCallback()
   }
 
+  addEvent = () => {
+    fb.database().ref('timeline/' + Math.random().toString(36).substr(2, 5)).set({
+      description: 'description',
+      start_date: 'start_date',
+      end_date : 'end_date',
+      keywords: {
+        0: 'zero',
+        1: 'one'
+      },
+      name: 'name',
+      related_companies: {
+        'ben': 'rohald'
+      }
+    });
+
+    this.handleClose()
+  }
+
   render () {
     const { classes } = this.props
 
     return (
       <Dialog open={this.props.isOpen} onClose={this.handleClose} aria-labelledby="simple-dialog-title" >
         <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
+        <DialogContent> 
+          <Button onClick={() => this.addEvent()}>Add Event</Button>
+        </DialogContent>
       </Dialog>
     )
   }
