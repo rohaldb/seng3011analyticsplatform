4070562cfa61238476888beb4eecf0070b9eb4dd
Dominic Fung <domokun997@gmail.com>
2018-05-25 22:25:44 +1000

    Limit rendering to a single company logo (first one in related_companies)

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -377,8 +377,8 @@ class Timeline extends React.Component {
                                   </Link>
                                 </Grid>
                                 <Grid container xs={2} direction="column" alignItems="flex-end">
-                                  { _.map(sortedEvents[k].related_companies, (c, i) => {
-                                    const logoUrl = `https://logo.clearbit.com/${_.toLower(_.split(i, /\s+/)[0])}.com?size=50`;
+                                  { _.map(_.take(_.keys(sortedEvents[k].related_companies), 1), (c, i) => { // Take first company's logo - adjust 2nd arg of _.take for more logos
+                                    const logoUrl = `https://logo.clearbit.com/${_.toLower(_.split(c, /\s+/)[0])}.com?size=50`;
                                     return (<img key={i} src={logoUrl} alt='' />);
                                   })}
                                 </Grid>
